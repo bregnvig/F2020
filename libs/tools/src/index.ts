f87164ae8d8af4205cf3a108d82f07d0f72e34a4
Flemming Bregnvig <flemming@bregnvig.dk>
2023-02-18 21:13:46 +0100

    More utils

--- libs/tools/src/index.ts
+++ libs/tools/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/array-utils';
 export * from './lib/compare-utils';
 export * from './lib/rxjs-utils';
+export * from './lib/utils';
 
--- libs/tools/src/lib/rxjs-utils.ts
+++ libs/tools/src/lib/rxjs-utils.ts
@@ -1,8 +1,10 @@
 import { pipe } from "rxjs";
 import { filter, map, pairwise, shareReplay, startWith } from 'rxjs/operators';
 import { deepCompare as deepCompareDefault } from './compare-utils';
+import { isNullish } from "./utils";
 
 export const truthy = <T>() => pipe(filter((a: T) => !!a));
+export const notNullish = <T>() => pipe(filter((a: T) => !isNullish(a)));
 export const falshy = <T>() => pipe(filter((a: T) => !a));
 export const shareLatest = <T>() => pipe(shareReplay<T>({ refCount: true, bufferSize: 1 }));
 
