ccbccb336b322f82585ca250d932aad8bfd6277c
Flemming Bregnvig <flemming@bregnvig.dk>
2023-03-10 08:47:49 +0100

    News

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -8,7 +8,6 @@ import { icon } from '@f2020/shared';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WhatElseComponent {
-  icon = icon.falRocketLaunch;
-  apple = icon.fabApple;
+  icon = icon.fabCss3;
   newPlayer = icon.farUserPlus;
 }
