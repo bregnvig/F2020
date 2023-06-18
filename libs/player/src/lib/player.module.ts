ebb1be36206f0098f4771bd7131029f177fe9bea
Flemming Bregnvig <flemming@bregnvig.dk>
2023-06-18 15:11:31 +0200

    Cleanup modules

--- libs/player/src/lib/player.module.ts
+++ libs/player/src/lib/player.module.ts
@@ -1,26 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { PlayerApiModule, PlayersApiModule } from '@f2020/api';
 import { ProfileComponent } from './component/profile/profile.component';
 
-const MaterialModule = [
-  MatListModule,
-  MatButtonModule,
-  MatToolbarModule,
-  MatSlideToggleModule,
-  MatIconModule,
-];
-
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule,
     PlayerApiModule,
     RouterModule.forChild([
       {
@@ -30,7 +16,6 @@ const MaterialModule = [
     ]),
     PlayerApiModule,
     PlayersApiModule,
-    ProfileComponent,
   ]
 })
 export class PlayerModule {
