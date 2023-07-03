3bae96d6a619dbb435876255e14852140c30723f
Flemming Bregnvig <flemming@bregnvig.dk>
2023-07-03 20:52:26 +0200

    With news

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -1,24 +1,25 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
-import { icon } from '@f2020/shared';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { icon } from '@f2020/shared';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-const versionNo = '4';
+const versionNo = '5';
 
 @Component({
-    selector: 'f2020-what-else',
-    templateUrl: './what-else.component.html',
-    styleUrls: ['./what-else.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [MatCardModule, FontAwesomeModule, RouterLink, MatButtonModule]
+  selector: 'f2020-what-else',
+  templateUrl: './what-else.component.html',
+  styleUrls: ['./what-else.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatCardModule, FontAwesomeModule, RouterLink, MatButtonModule]
 })
 export class WhatElseComponent {
 
   @HostBinding('hidden') isHidden = localStorage.getItem('what-else') === versionNo;
-  icon = icon.falRocketLaunch;
+  // icon = icon.falRocketLaunch;
+  icon = icon.farFaceSadCry;
   tireFlat = icon.falTireFlat;
 
   dismissMessage() {
