ea614bfc91a264b525d52c6006ee6aeca0c5a3a4
Flemming Bregnvig <flemming@bregnvig.dk>
2023-07-06 16:14:15 +0200

    unfreeze handles arrays

--- libs/tools/src/lib/utils.ts
+++ libs/tools/src/lib/utils.ts
@@ -1,7 +1,10 @@
 export type nullish = null | undefined;
 export const isNullish = <T>(value: T | nullish): value is T => value === null || value === undefined;
 export const unfreeze = <T>(value: T): T => {
-  if (typeof value !== 'object' || value['constructor']['name'] !== 'Object') {
+  if (Array.isArray(value)) {
+    return value.map(unfreeze) as unknown as T;
+  }
+  if (value === null || typeof value !== 'object' || value['constructor']?.['name'] !== 'Object') {
     return value;
   }
   return Object.fromEntries(Object.entries(value || {}).map(([key, value]) => [key, unfreeze(value)])) as T;
