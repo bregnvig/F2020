8756eb7f12036d25ce5d7b1517c6b0ee5b291597
Flemming Bregnvig <flemming@bregnvig.dk>
2023-07-07 08:19:35 +0200

    Stop using transform

--- libs/standing/src/lib/component/standing-driver/number-card/number-card.component.ts
+++ libs/standing/src/lib/component/standing-driver/number-card/number-card.component.ts
@@ -1,18 +1,18 @@
-import { ChangeDetectionStrategy, Component, Input, numberAttribute } from '@angular/core';
-import { IconName, IconPrefix } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconName, IconPrefix } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
-    selector: 'f2020-number-card',
-    templateUrl: './number-card.component.html',
-    styleUrls: ['./number-card.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [MatCardModule, FontAwesomeModule],
+  selector: 'f2020-number-card',
+  templateUrl: './number-card.component.html',
+  styleUrls: ['./number-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatCardModule, FontAwesomeModule],
 })
 export class NumberCardComponent {
   @Input() title: string;
   @Input() icon: [IconPrefix, IconName];
-  @Input({transform: numberAttribute}) number: number;
+  @Input() number: string | number;
 }
