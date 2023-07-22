f539e5fb844de023fb01cd85e83b0700d4cf764b
Flemming Bregnvig <flemming@bregnvig.dk>
2023-07-22 13:10:51 +0200

    Create participants when bid is being started

--- apps/cloud/src/app/bid/bid-started.trigger.ts
+++ apps/cloud/src/app/bid/bid-started.trigger.ts
@@ -1,12 +1,13 @@
 import { Bid, Player } from '@f2020/data';
 import { firestore } from 'firebase-admin';
+import { DocumentReference } from 'firebase-admin/firestore';
 import { region } from 'firebase-functions/v1';
 import { DocumentSnapshot } from 'firebase-functions/v1/firestore';
-import { sendNotification } from '../../lib';
+import { currentSeason, getCurrentRace, racesURL, seasonsURL, sendNotification } from '../../lib';
 ;
 
 export const newBidTrigger = region('europe-west1').firestore.document('seasons/{seasonId}/races/{raceId}/bids/{userId}')
-  .onCreate(async (snapshot: DocumentSnapshot, context) => {
+  .onCreate(async (snapshot: DocumentSnapshot) => {
     const bid: Partial<Bid> = snapshot.data() as Partial<Bid>;
 
     const db = firestore();
@@ -20,5 +21,16 @@ export const newBidTrigger = region('europe-west1').firestore.document('seasons/
       .then(playerSnapshot => playerSnapshot.docs.map(d => d.data() as Player))
       .then(_players => _players.filter(allFilter));
 
-    return Promise.all(players.map(p => sendNotification(p.tokens, `🥳 Bud på vej!`, `${bid.player?.displayName} er ved at lave sit bud!`)));
+    const season = await currentSeason();
+    const race = await getCurrentRace('open');
+
+    return Promise.all([
+      ...players.map(p => sendNotification(p.tokens, `🥳 Bud på vej!`, `${bid.player?.displayName} er ved at lave sit bud!`)),
+      db.runTransaction(transaction => {
+        const doc = db.doc(`${seasonsURL}/${season.id}/${racesURL}/${race.round}/participants/${bid.player.uid}`) as DocumentReference<{ player: Player, submitted: false; }>;
+        transaction.set(doc, { player: bid.player, submitted: false });
+        return Promise.resolve('Bid without data written');
+      })
+
+    ]);
   });    
