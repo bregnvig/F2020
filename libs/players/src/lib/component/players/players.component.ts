61533341c3eccfd095446f76da06f648b5e4761a
Flemming Bregnvig <flemming@bregnvig.dk>
2024-03-03 10:09:12 +0100

    Add selector

--- libs/players/src/lib/component/players/players.component.ts
+++ libs/players/src/lib/component/players/players.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { PlayersStore } from '@f2020/api';
 
 @Component({
+  selector: 'f2020-players',
   template: `
     <router-outlet></router-outlet>`,
   standalone: true,
