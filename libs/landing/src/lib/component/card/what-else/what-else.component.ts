9a93beaa4d63ff9e03f514e9e822454783184ed8
Flemming Bregnvig <flemming@nestech.dk>
2024-04-03 16:56:54 +0200

    Updated what's new

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -5,12 +5,11 @@ import { RouterLink } from '@angular/router';
 import { icon } from '@f2020/shared';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-const versionNo = '12';
+const versionNo = '14';
 
 @Component({
   selector: 'f2020-what-else',
   templateUrl: './what-else.component.html',
-  styleUrls: ['./what-else.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [MatCardModule, FontAwesomeModule, RouterLink, MatButtonModule],
