829650c2fc62e9d8560c7728a1d15227df9e52e5
Flemming Bregnvig <flemming@nestech.dk>
2024-04-06 16:09:01 +0200

    Remove the test checking

--- apps/functions/src/lib/message.service.ts
+++ apps/functions/src/lib/message.service.ts
@@ -1,24 +1,25 @@
 import { log } from 'firebase-functions/logger';
 import { getMessaging } from 'firebase-admin/messaging';
 
-export const sendNotification = (tokens: string[], title: string, body: string, data?: { [key: string]: string; }): Promise<any> => {
-  return getMessaging().sendEachForMulticast({
-    data,
-    tokens,
-    notification: {
-      title,
-      body,
-    },
-    webpush: {
+export const sendNotification = async (tokens: string[], title: string, body: string, data?: { [key: string]: string; }): Promise<any> => {
+  try {
+    const response = await getMessaging().sendEachForMulticast({
+      data,
+      tokens,
       notification: {
-        badge: 'https://f2020.bregnvig.dk/assets/messaging/badge.v2.png',
-        icon: 'https://f2020.bregnvig.dk/assets/icons/icon-192x192.png',
+        title,
+        body,
       },
-    },
-  }).then((response) => {
+      webpush: {
+        notification: {
+          badge: 'https://f2020.bregnvig.dk/assets/messaging/badge.v2.png',
+          icon: 'https://f2020.bregnvig.dk/assets/icons/icon-192x192.png',
+        },
+      },
+    });
     // Response is a message ID string.
     log('Successfully sent message:', response);
-  }).catch((error) => {
+  } catch (error) {
     log('Error sending message:', error);
-  });
+  }
 };
