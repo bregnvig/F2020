207ba0484190e00054ec0cada0ce7cd7564a199e
Flemming Bregnvig <flemming@nestech.dk>
2024-04-10 08:23:31 +0200

    2. generation news

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 import { icon } from '@f2020/shared';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-const versionNo = '15';
+const versionNo = '16';
 
 @Component({
   selector: 'f2020-what-else',
