95b1e5eb4275ce86a482e363b64c91625de7254b
Flemming Bregnvig <flemming@nestech.dk>
2024-05-09 09:10:00 +0200

    Check for close every hours

--- apps/functions/src/app/race/close-race.crontab.ts
+++ apps/functions/src/app/race/close-race.crontab.ts
@@ -1,22 +1,22 @@
-import { DateTime } from 'luxon';
-import { getCurrentRace, logAndCreateError, updateRace } from '../../lib';
 import { log } from 'firebase-functions/logger';
 import { onSchedule } from 'firebase-functions/v2/scheduler';
+import { DateTime } from 'luxon';
+import { getCurrentRace, logAndCreateError, updateRace } from '../../lib';
 
 
 // This will be run every friday at 11.00 Europe/Copenhagen!
 export const closeRaceCrontab = onSchedule({
-    timeZone: 'Europe/Copenhagen',
-    schedule: '0 11 * * *',
-  }, async () => getCurrentRace('open')
-    .then(async race => {
-      if (race) {
-        log(`Is ${race.name} closed`, race.close <= DateTime.local());
-        if (race.close <= DateTime.local()) {
-          return updateRace(race.season, race.round, { state: 'closed' });
-        }
-        return Promise.resolve(undefined);
+  timeZone: 'Europe/Copenhagen',
+  schedule: '0 * * * *',
+}, async () => getCurrentRace('open')
+  .then(async race => {
+    if (race) {
+      log(`Is ${race.name} closed`, race.close <= DateTime.local());
+      if (race.close <= DateTime.local()) {
+        return updateRace(race.season, race.round, { state: 'closed' });
       }
-      throw logAndCreateError('not-found', 'No race');
-    }),
+      return Promise.resolve(undefined);
+    }
+    throw logAndCreateError('not-found', 'No race');
+  }),
 );
