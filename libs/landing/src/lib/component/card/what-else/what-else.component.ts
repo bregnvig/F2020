ade5f8da79fe95963a395be6c38ea8ecd25a1762
Flemming Bregnvig <flemming@nestech.dk>
2024-06-16 12:56:24 +0200

    Update news

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 import { icon } from '@f2020/shared';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-const versionNo = '17';
+const versionNo = '18';
 
 @Component({
   selector: 'f2020-what-else',
