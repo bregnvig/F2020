dcaccb9a5167fdc584c3b147b1d2e086ea1e9aa1
Flemming Bregnvig <flemming@bregnvig.dk>
2024-09-14 14:18:49 +0200

    Add new environment

--- apps/ui/src/environments/environment.prod.ts
+++ apps/ui/src/environments/environment.prod.ts
@@ -2,14 +2,12 @@ export const environment = {
   production: true,
   useEmulator: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCz1m3SvBz1OpVWLFWX-FL-SCY_8HbgxEE",
-    authDomain: "f2020.bregnvig.dk",
-    databaseURL: "https://f1-2020-9dec0.firebaseio.com",
-    projectId: "f1-2020-9dec0",
-    storageBucket: "f1-2020-9dec0.appspot.com",
-    messagingSenderId: "209105975677",
-    appId: "1:209105975677:web:a18d0a67d142e0e85f3414",
-    measurementId: "G-BRDT1FLJ41",
-    vapidKey: 'BBSlUc8IHB5zv1HkWXvuesGCGZZmRX3A0mDkV5c5qnCiVj3Vt3jJhcD9ldHsouZpjv3N3XXNfz5xSKB097SjdPk'
-  }
+    apiKey: 'AIzaSyAK9_kQTf7Lm7TdbYqDP_nFo7fwWvuHfLg',
+    authDomain: 'f1-2024-8ab8e.firebaseapp.com',
+    projectId: 'f1-2024-8ab8e',
+    storageBucket: 'f1-2024-8ab8e.appspot.com',
+    messagingSenderId: '651727325900',
+    appId: '1:651727325900:web:aec3360693b0b39516d069',
+    vapidKey: 'BFnBh8HKN7mhvwA-auVC54sAjaHFBR2gJ6GxtwdW5LxeFfOGztsW84VMlqP9Szzpkhq2MBCA1NXh53BLaUNFc4c',
+  },
 };
