30dbea0f3a17968efbda33e443e071036776ced5
Flemming Bregnvig <flemming@bregnvig.dk>
2024-10-17 10:05:29 +0200

    Add more log

--- libs/api/src/lib/player/service/fcm.service.ts
+++ libs/api/src/lib/player/service/fcm.service.ts
@@ -17,21 +17,25 @@ export class FCMService {
     } else {
       const messaging = inject(Messaging);
       this.setupMessaging = (): Promise<void | string> => {
-        if (Notification.permission === 'granted') {
-          return Promise.resolve();
-        } else if (Notification.permission === 'denied') {
+        if (Notification.permission === 'denied') {
           return Promise.reject('Notification permission denied');
         }
         return new Promise((resolve, reject) => {
           setTimeout(() => {
-            firstValueFrom(this.snackBar.open('Hvis du vil modtage påmindelse, løbsresultater etc, så skal du godkende at vi må sende notifikationer til dig 👍', 'OK').onAction()).then(
+            const isAlreadyGranted = Notification.permission === 'granted';
+            (isAlreadyGranted
+                ? Promise.resolve()
+                : firstValueFrom(this.snackBar.open('Hvis du vil modtage påmindelse, løbsresultater etc, så skal du godkende at vi må sende notifikationer til dig 👍', 'OK').onAction())
+            ).then(
               async () => {
                 await navigator.serviceWorker.register('/assets/firebase-messaging-sw.js', {
                   type: 'module',
                 }).then(serviceWorkerRegistration => getToken(messaging, {
                   serviceWorkerRegistration,
-                }).then(token => resolve(token)))
-                  .catch(error => reject(error));
+                }).then(token => {
+                  isAlreadyGranted && console.log('Already granted', token);
+                  resolve(isAlreadyGranted ? undefined : token);
+                }).catch(error => reject(error)));
               },
             );
           });
