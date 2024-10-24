c6202d90c92a4e220f80c7d31f41f847462d7298
Flemming Bregnvig <flemming@bregnvig.dk>
2024-10-24 08:21:27 +0200

    Export races service

--- libs/api/src/index.ts
+++ libs/api/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/player/service/player-api.service';
 export * from './lib/players/+state';
 export * from './lib/players/service/players-api.service';
 export * from './lib/race/+state';
+export * from './lib/race/service/races.service';
 export * from './lib/season/+state';
 export * from './lib/drivers';
 export * from './lib/service';
