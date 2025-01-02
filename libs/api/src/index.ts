cd23f6bc6915aeaa8fd09a0f0fab745c7f5f518f
Flemming Bregnvig <flemming@nestech.dk>
2025-01-02 07:14:20 +0100

    Export result builder

--- libs/api/src/index.ts
+++ libs/api/src/index.ts
@@ -4,6 +4,7 @@ export * from './lib/players/+state';
 export * from './lib/players/service/players-api.service';
 export * from './lib/race/+state';
 export * from './lib/race/service/races.service';
+export * from './lib/race/service/result-builder';
 export * from './lib/season/+state';
 export * from './lib/drivers';
 export * from './lib/service';
