607bc0eaa61511146212f1087d14c729305f74f8
Flemming Bregnvig <flemming@bregnvig.dk>
2025-01-04 13:37:13 +0100

    Only 3 thousand decimals

--- libs/shared/src/lib/pipe/pole-position-time.pipe.ts
+++ libs/shared/src/lib/pipe/pole-position-time.pipe.ts
@@ -2,15 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { mapper } from '@f2020/data';
 
 @Pipe({
-    name: 'polePositionTime',
-    standalone: true
+  name: 'polePositionTime',
+  standalone: true,
 })
 export class PolePositionTimePipe implements PipeTransform {
 
   transform(value: number): string {
     if (typeof value === 'number') {
       const pole = mapper.polePosition.split(value);
-      return `${pole.minutes}:${pole.seconds.toString().padStart(2, '0')}.${pole.milliseconds.toString().padStart(3, '0')}`;
+      return `${pole.minutes}:${pole.seconds.toString().padStart(2, '0')}.${pole.milliseconds.toString().padStart(3, '0').substring(0, 3)}`;
     }
     return value;
   }
