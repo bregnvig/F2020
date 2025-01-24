45c4da9a24191a5433515ed54e3ce47be52004ba
Flemming Bregnvig <flemming@bregnvig.dk>
2025-01-24 14:32:14 +0100

    Add AI generated messages

--- apps/functions/src/app/result/result-notification.trigger.ts
+++ apps/functions/src/app/result/result-notification.trigger.ts
@@ -1,42 +1,166 @@
-import { WBC, WBCResult } from '@f2020/data';
+import { Bid, ISeason, WBC, WBCResult } from '@f2020/data';
 import { log } from 'firebase-functions/logger';
 import { onDocumentUpdated } from 'firebase-functions/v2/firestore';
-import { sendMail, sendNotification } from '../../lib';
+import { collectionPaths, sendMail, sendNotification } from '../../lib';
+import { getFirestore } from 'firebase-admin/lib/firestore';
+import OpenAI from 'openai';
+import * as process from 'node:process';
 
-const mailBody = (playerName: string, wbcPoints: number, raceName: string) =>
-  `<h3>Hej ${playerName}</h3>
+const wbcPointsToPosition = {
+  25: 'første',
+  18: 'anden',
+  15: 'tredje',
+};
+
+const wbcPointsToDescription = {
+  25: (bids: Bid[]) => `${bids[0].player.displayName} fik ${bids[0].points} point, mens ${bids[1].player.displayName} kom på anden pladsen med ${bids[1].points} point`,
+  18: (bids: Bid[]) => `${bids[1].player.displayName} kom på anden pladsen med ${bids[1].points} point. Første pladsen gik til ${bids[0].player.displayName} med ${bids[0].points} point, mens tredje pladsen gik til ${bids[2].player.displayName} som fik ${bids[2].points} point`,
+  15: (bids: Bid[]) => `${bids[2].player.displayName} kom på tredje pladsen med ${bids[2].points} point. Første pladsen gik til ${bids[0].player.displayName} med ${bids[0].points} point, mens anden pladsen gik til ${bids[1].player.displayName} som fik ${bids[1].points} point`,
+};
+
+const userChat = (name: string, raceName: string, wbcPoints: number, bids: Bid[]) => `
+  Skriv besked til ${name} der kom på ${wbcPointsToPosition[wbcPoints]} pladsen løbet i ${raceName}. ${wbcPointsToDescription[wbcPoints](bids)}
+`;
+
+const aiGeneratedMailMessage = async (playerName: string, raceName: string, wbcPoints: number, bids: Bid[]): Promise<{ subject: string, body: string }> => {
+  const openai = new OpenAI({
+    apiKey: process.env.openAIApiKey,
+    organization: process.env.openAIOrganization,
+    project: process.env.openAIProject,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    messages: [
+      {
+        role: 'system',
+        content: 'Du laver morsomme e-mail der beskriver deltagerens første, anden eller tredje plads i et F1 væddemål',
+      },
+      {
+        role: 'user',
+        content: userChat(playerName, raceName, wbcPoints, bids),
+      },
+    ],
+    response_format: {
+      type: 'json_schema',
+      json_schema: {
+        name: 'congratulation_schema',
+        schema: {
+          type: 'object',
+          properties: {
+            subject: {
+              description: 'Emne til en email',
+              type: 'string',
+            },
+            body: {
+              description: 'Beskeden. Skal afsluttes med newline og så Wrouum, F1emming',
+              type: 'string',
+            },
+          },
+          additionalProperties: false,
+        },
+      },
+    },
+  });
+  return JSON.parse(response.choices[0].message.content);
+};
+
+const aiGeneratedNoticationMessage = async (playerName: string, raceName: string, index: number): Promise<{ title: string, body: string }> => {
+  const openai = new OpenAI({
+    apiKey: process.env.openAIApiKey,
+    organization: process.env.openAIOrganization,
+    project: process.env.openAIProject,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    messages: [
+      {
+        role: 'system',
+        content: 'Du laver morsomme en morsom smart phone notifikations title og body der beskriver deltagerens første anden eller tredje plads i et F1 væddemål. Du bliver oplyst følgende information navn på deltager, navn på løb, plcering. Notifikationen skal være på dansk',
+      },
+      {
+        role: 'user',
+        content: `${playerName}, ${raceName}, ${index + 1}`,
+      },
+    ],
+    response_format: {
+      // See /docs/guides/structured-outputs
+      type: 'json_schema',
+      json_schema: {
+        name: 'congratulation_schema',
+        schema: {
+          type: 'object',
+          properties: {
+            subject: {
+              title: 'Titlen på notifikationen',
+              type: 'string',
+            },
+            body: {
+              description: 'Notifikations beskeden',
+              type: 'string',
+            },
+          },
+          additionalProperties: false,
+        },
+      },
+    },
+  });
+  return JSON.parse(response.choices[0].message.content);
+};
+
+const mailBody = (playerName: string, wbcPoints: number, raceName: string) => {
+
+  return `<h3>Hej ${playerName}</h3>
      <div> 
       <p> ${raceName} er nu afgjort - du har fået ${wbcPoints} WBC points</p>
      </div>     
                   
      Wroouumm,<br/>
      F1emming`;
+};
 
-const messageBody = (raceName: string, wbcPoints: number) => `${raceName} er nu afgjort - du har fået ${wbcPoints} WBC points`;
+const notificationBody = (raceName: string, wbcPoints: number) => `${raceName} er nu afgjort - du har fået ${wbcPoints} WBC points`;
 
 export const resultNotificationTrigger = onDocumentUpdated('seasons/{seasonId}', async event => {
   const before: WBC = event.data.before.data()?.wbc || [];
-  const after: WBC = event.data.after.data()?.wbc || [];
-  if ((after.results?.length && (before.results?.length ?? 0) < (after.results?.length ?? 0))) {
+  const season = event.data.after.data() as ISeason;
+  const after: WBC = season?.wbc;
+  if ((after?.results?.length && (before.results?.length ?? 0) < (after.results?.length ?? 0))) {
     const result: WBCResult = after.results.find(r => !before.results?.some(({ round }) => round === r.round));
+    const db = getFirestore();
+    const raceResults = await db.collection(collectionPaths.bids(season.id, result.round)).where('submitted', '==', true).get()
+      .then(snapshot => snapshot.docs)
+      .then(snapshots => snapshots.map(s => s.data() as Bid))
+      .then(raceResults => raceResults.sort((a, b) => b.points - a.points));
     log('Race', result.raceName, 'Is now completed - lets send notifications');
-    return Promise.all(result.players.map(async element => {
+    return Promise.all(result.players.map(async (element, index) => {
       const sendWBCResult = async (place: string, badge?: string) => {
-        await sendMail(element.player.email, place, mailBody(element.player.displayName, element.points, result.raceName)).then((msg) => {
+        let mail = {
+          subject: place,
+          body: mailBody(element.player.displayName, element.points, result.raceName),
+        };
+        let notification = {
+          title: place,
+          body: notificationBody(result.raceName, element.points),
+        };
+        if (index < 3) {
+          mail = await aiGeneratedMailMessage(element.player.displayName, result.raceName, element.points, raceResults);
+          notification = await aiGeneratedNoticationMessage(element.player.displayName, result.raceName, index);
+        }
+        await sendMail(element.player.email, mail.subject, mail.body).then((msg) => {
           log(`Mail result :(${msg})`);
         });
         if (element.player.tokens?.length) {
-          await sendNotification(element.player.tokens, place, messageBody(result.raceName, element.points), badge);
+          await sendNotification(element.player.tokens, notification.title, notification.body, badge).then(msg => {
+            log(`Notification result :(${msg})`);
+          });
         }
       };
-      if ([12, 10, 8, 6, 4, 2, 1].indexOf(element.points) > -1) {
+      if ([25, 18, 15].includes(element.points)) {
+        await sendWBCResult('ai generated', 'https://f1.bregnvig.dk/assets/messaging/trophy.png');
+      } else if ([12, 10, 8, 6, 4, 2, 1].includes(element.points)) {
         await sendWBCResult('😒 Selvom du ikke kom i top tre - så fik du da points :-)');
-      } else if (element.points === 25) {
-        await sendWBCResult('🥇 Tillykke med din første plads :-)', 'https://f1.bregnvig.dk/assets/messaging/trophy.png');
-      } else if (element.points === 18) {
-        await sendWBCResult('🥈 Tillykke med din anden plads :-)', 'https://f1.bregnvig.dk/assets/messaging/trophy.png');
-      } else if (element.points === 15) {
-        await sendWBCResult('🥉 Tillykke med din tredje plads :-)', 'https://f1.bregnvig.dk/assets/messaging/trophy.png');
       } else {
         await sendWBCResult('🫣 Æv du fik ingen points  :-(');
       }
