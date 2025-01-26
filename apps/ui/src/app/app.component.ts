127e2a21e5cd83f8e1dd2396e950d496deeb975c
Flemming Bregnvig <flemming@bregnvig.dk>
2025-01-26 14:53:48 +0100

    Remove allow write for effects

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -12,18 +12,18 @@ import { filter, first, switchMap } from 'rxjs/operators';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
-    selector: 'f2020-root',
-    templateUrl: './app.component.html',
-    imports: [
-        MatToolbarModule,
-        RouterLink,
-        MatButtonModule,
-        MatIconModule,
-        MatSidenavModule,
-        SidebarComponent,
-        RouterOutlet,
-        FontAwesomeModule,
-    ]
+  selector: 'f2020-root',
+  templateUrl: './app.component.html',
+  imports: [
+    MatToolbarModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    SidebarComponent,
+    RouterOutlet,
+    FontAwesomeModule,
+  ],
 })
 export class AppComponent {
 
@@ -52,7 +52,7 @@ export class AppComponent {
           this.router.navigate(['info', 'roles']);
         }
       }
-    }, { allowSignalWrites: true });
+    });
   }
 
   private checkForVersionUpdate() {
