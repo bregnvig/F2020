40b6ae2301825c7ce8538bc5f6cef965b1ffa892
Flemming Bregnvig <flemming@nestech.dk>
2025-07-08 09:24:19 +0200

    Update what else

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -4,7 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { icon } from '@f2020/shared';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
-const versionNo = '32';
+const versionNo = '33';
 
 @Component({
   selector: 'f2020-what-else',
