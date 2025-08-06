110b24051bbfcc27387421b394dd52404631fa10
Flemming Bregnvig <flemming@bregnvig.dk>
2025-08-06 09:37:17 +0200

    Add the interval url

--- libs/openf1/src/lib/openf1-urls.ts
+++ libs/openf1/src/lib/openf1-urls.ts
@@ -1,6 +1,7 @@
 export const openF1Url = {
   session: (seasonId: string | number, circuitKey: number, sessionName: 'Race' | 'Qualifying' | 'Sprint') => `https://api.openf1.org/v1/sessions?year=${seasonId}&circuit_key=${circuitKey}&session_name=${sessionName}`,
   positions: (sessionKey: number) => `https://api.openf1.org/v1/position?session_key=${sessionKey}`,
+  intervals: (sessionKey: number) => `https://api.openf1.org/v1/intervals?session_key=${sessionKey}`,
   labs: (sessionKey: number) => `https://api.openf1.org/v1/laps?session_key=${sessionKey}`,
   pitStops: (sessionKey: number) => `https://api.openf1.org/v1/pit?session_key=${sessionKey}`,
   sessionResults: (sessionKey: number) => `https://api.openf1.org/v1/session_result?session_key=${sessionKey}`,
