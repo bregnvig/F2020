7314543542527777cf7723be4cfb095038509adf
Flemming Bregnvig <flemming@bregnvig.dk>
2025-09-06 10:02:06 +0200

    Fix mapper

--- libs/data/src/lib/mapper/sector.mapper.ts
+++ libs/data/src/lib/mapper/sector.mapper.ts
@@ -31,16 +31,22 @@ const openF1Map = ({ laps, drivers }: OpenF1SectorParams): IDriverSector[] => {
     const current = acc.get(lap.driver_number) || { sector1: undefined, sector2: undefined, sector3: undefined };
 
     return acc.set(lap.driver_number, {
-      sector1: isNotNullish(lap.duration_sector_1) && isNotNullish(current.sector1) ? Math.min(current.sector1, lap.duration_sector_1) : current.sector1,
-      sector2: isNotNullish(lap.duration_sector_2) && isNotNullish(current.sector2) ? Math.min(current.sector2, lap.duration_sector_2) : current.sector2,
-      sector3: isNotNullish(lap.duration_sector_3) && isNotNullish(current.sector3) ? Math.min(current.sector3, lap.duration_sector_3) : current.sector3,
+      sector1: isNotNullish(lap.duration_sector_1) ? 
+        (isNotNullish(current.sector1) ? Math.min(current.sector1, lap.duration_sector_1) : lap.duration_sector_1) : 
+        current.sector1,
+      sector2: isNotNullish(lap.duration_sector_2) ? 
+        (isNotNullish(current.sector2) ? Math.min(current.sector2, lap.duration_sector_2) : lap.duration_sector_2) : 
+        current.sector2,
+      sector3: isNotNullish(lap.duration_sector_3) ? 
+        (isNotNullish(current.sector3) ? Math.min(current.sector3, lap.duration_sector_3) : lap.duration_sector_3) : 
+        current.sector3,
     });
   }, new Map<number, { sector1: number | undefined; sector2: number | undefined; sector3: number | undefined; }>());
 
-  const getSectorColor = (duration: number | undefined, personalBest: number, best: number): SectorColor | undefined => {
+  const getSectorColor = (duration: number | undefined, personalBest: number | undefined, best: number): SectorColor | undefined => {
     if (!duration) return;
     if (duration === best) return 'purple';
-    if (duration === personalBest) return 'green';
+    if (personalBest && duration === personalBest) return 'green';
     return 'yellow';
   };
 
@@ -50,15 +56,15 @@ const openF1Map = ({ laps, drivers }: OpenF1SectorParams): IDriverSector[] => {
     return {
       driver,
       sector1: {
-        status: getSectorColor(lap.duration_sector_1, driverSectors?.sector1 ?? lap.duration_sector_1, fastestSector1),
+        status: getSectorColor(lap.duration_sector_1, driverSectors?.sector1, fastestSector1),
         mini: lap.segments_sector_1?.map(status => SectorColorMap[status]),
       },
       sector2: {
-        status: getSectorColor(lap.duration_sector_2, driverSectors?.sector2 ?? lap.duration_sector_2, fastestSector2),
+        status: getSectorColor(lap.duration_sector_2, driverSectors?.sector2, fastestSector2),
         mini: lap.segments_sector_2?.map(status => SectorColorMap[status]),
       },
       sector3: {
-        status: getSectorColor(lap.duration_sector_3, driverSectors?.sector3 ?? lap.duration_sector_3, fastestSector3),
+        status: getSectorColor(lap.duration_sector_3, driverSectors?.sector3, fastestSector3),
         mini: lap.segments_sector_3?.map(status => SectorColorMap[status]),
       },
     } as IDriverSector;
