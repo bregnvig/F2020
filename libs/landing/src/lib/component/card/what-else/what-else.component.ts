91e30844af4b908d9836b012f21a04a77f614425
Flemming Bregnvig <flemming@bregnvig.dk>
2025-09-29 21:02:14 +0200

    Latest news

--- libs/landing/src/lib/component/card/what-else/what-else.component.ts
+++ libs/landing/src/lib/component/card/what-else/what-else.component.ts
@@ -4,7 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { icon } from '@f2020/shared';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
-const versionNo = '34';
+const versionNo = '35';
 
 @Component({
   selector: 'f2020-what-else',
