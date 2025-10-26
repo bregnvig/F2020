349f4c9347c376936f05290a59cbf2828ab6f9c0
Flemming Bregnvig <flemming@bregnvig.dk>
2025-10-26 12:47:49 +0100

    Accelerate

--- libs/shared/src/lib/component/loading/loading.component.ts
+++ libs/shared/src/lib/component/loading/loading.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 const random = (max: number): number => Math.floor(Math.random() * Math.floor(max));
 
@@ -11,18 +12,22 @@ const tyres: string[] = [
 ];
 
 @Component({
-    selector: 'sha-loading',
-    template: `
+  selector: 'sha-loading',
+  template: `
     <div>
-      <img [src]="tyre" alt="loading">
+      <img width="128" height="128" [ngSrc]="tyre" alt="loading">
     </div>
   `,
-    styleUrls: ['./loading.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true
+  styleUrls: ['./loading.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+  ],
 })
 export class LoadingComponent {
   readonly tyre: string;
+
   constructor() {
     this.tyre = `assets/loading/${tyres[random(5)]}.svg`;
   }
