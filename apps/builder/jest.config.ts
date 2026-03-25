3821cf3892cf893b79acd0597a2add16f11e6d8f
Flemming Bregnvig <flemming@bregnvig.dk>
2026-03-25 08:04:54 +0100

    Updated to latest. Removed jest and cypress

--- apps/builder/jest.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export default {
-  preset: "../../jest.preset.js",
-  coverageDirectory: "../../coverage/apps/builder",
-  globals: { "ts-jest": { tsconfig: "<rootDir>/tsconfig.spec.json" } },
-  displayName: "builder",
-  testEnvironment: "node",
-};
